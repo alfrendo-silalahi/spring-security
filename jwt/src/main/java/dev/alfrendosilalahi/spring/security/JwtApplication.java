5d6a49915afef15b8328cc382a8c23d92c889deb
alfrendo-silalahi <alfrendos72@gmail.com>
2024-10-10 22:38:16 +0700

    feat: Refactor project directory

--- jwt/src/main/java/dev/alfrendosilalahi/spring/security/JwtApplication.java
+++ jwt/src/main/java/dev/alfrendosilalahi/spring/security/JwtApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSpringSecurityJWTApplication {
+public class JwtApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSpringSecurityJWTApplication.class, args);
+		SpringApplication.run(JwtApplication.class, args);
 	}
 
 }
